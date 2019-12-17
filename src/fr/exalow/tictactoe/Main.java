21a1a4dd0127bbbcae60357696339a41c212c45a
Exalow <53000164+Exalow@users.noreply.github.com>
2019-12-17 16:54:53 +0100

    Add files via upload

--- src/fr/exalow/tictactoe/Main.java
+++ src/fr/exalow/tictactoe/Main.java
@@ -2,9 +2,19 @@ package fr.exalow.tictactoe;
 
 import fr.exalow.tictactoe.model.Player;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        new TicTacToe(new Player("Joueur 1", 'O'), new Player("Joueur 2", 'X')).run();
+
+        final Scanner scan = new Scanner(System.in);
+
+        System.out.println("(Player 1) Name : ");
+        Player p1 = new Player(scan.nextLine(), 'X');
+        System.out.println("(Player 1) Name : ");
+        Player p2 = new Player(scan.nextLine(), 'O');
+
+        new TicTacToe(p1, p2);
     }
 }
